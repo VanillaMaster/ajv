5b548a6f7399b6d9b594c0559699a476384dac9b
Vanilla <vanillaflamaster@gmail.com>
2025-04-19 00:09:32 +0300

    used "arbitrary module namespace identifier names" only in cases when needed, to prevent breaking changes on pre-es2022

--- lib/standalone/index.ts
+++ lib/standalone/index.ts
@@ -2,7 +2,7 @@ import type AjvCore from "../core"
 import type {AnyValidateFunction, SourceCode} from "../types"
 import type {SchemaEnv} from "../compile"
 import {UsedScopeValues, UsedValueState, ValueScopeName, varKinds} from "../compile/codegen/scope"
-import {_, nil, _Code, Code, getProperty} from "../compile/codegen/code"
+import {_, nil, _Code, Code, getProperty, getEsmExportName} from "../compile/codegen/code"
 
 function standaloneCode(
   ajv: AjvCore,
@@ -48,7 +48,7 @@ function standaloneCode(
       if (v) {
         const vCode = validateCode(usedValues, v.source)
         const exportSyntax = ajv.opts.code.esm
-          ? _`export { ${v.source?.validateName} as ${name} };`
+          ? _`export { ${v.source?.validateName} as ${getEsmExportName(name)} };`
           : _`exports${getProperty(name)} = ${v.source?.validateName};`
         code = _`${code}${_n}${exportSyntax}${_n}${vCode}`
       }
